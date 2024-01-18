5484d77a4f9cfb57d8e171b067b959e3c582e6e2
Charudatta Potdar <charupot08@gmail.com>
2024-01-18 08:44:07 -0700

    extracted code

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,4 @@
 module.exports = (req, res) => {
-  if (req.method == "POST") {
-    // res.status(200).redirect("https://sync-wave-client.vercel.app/");
-    res.status(200).send("Code recieved");
-  }
+  res.json({ code: req.query.code });
+  return
 };
