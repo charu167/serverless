b7b5d116339ccb08e1bd64b38972f92cee405f93
Charudatta Potdar <charupot08@gmail.com>
2024-01-18 09:26:45 -0700

    redirecting to callbackspotify page

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
 //   res.send(req.query.code)
 
-  res.redirect(`https://sync-wave-client.vercel.app/?code=${req.query.code}`)
+  res.redirect(`https://sync-wave-client.vercel.app/callbackspotify?code=${req.query.code}`)
   return;
 };
