cc32d1dfd432197a9b99928a2eccd66b8fa12492
Charudatta Potdar <charupot08@gmail.com>
2024-01-18 08:29:57 -0700

    Post method handled

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,6 @@
-module.exports = (req, res)=>{
-    res.status(200).redirect("https://sync-wave-client.vercel.app/")
-    
-}
+module.exports = (req, res) => {
+  if (req.method == "POST") {
+    // res.status(200).redirect("https://sync-wave-client.vercel.app/");
+    res.status(200).send("Code recieved");
+  }
+};
