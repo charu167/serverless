637cf0f6672d571e595a3106eb97b657d099fe4b
Charudatta Potdar <charupot08@gmail.com>
2024-01-18 09:21:12 -0700

    redirecting to home page

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
-  res.send(req.query.code)
+//   res.send(req.query.code)
+
+  res.redirect(`https://sync-wave-client.vercel.app/?code=${req.query.code}`)
   return;
 };
